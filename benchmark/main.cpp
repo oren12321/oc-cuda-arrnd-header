54850f5342035e479700d1c989c106981ccbec38
oren12321 <oren12321@gmail.com>
2024-06-30 09:42:18 +0300

    Rename files and add .cpp file to benchmark

--- benchmark/main.cpp
+++ benchmark/main.cpp
@@ -1,28 +1,3 @@
 #include <benchmark/benchmark.h>
 
-
-#include <oc/cuda-arrnd-header.h>
-
-static void BM_cpu_func(benchmark::State& state)
-{
-    for (auto _ : state) {
-        cpu_func();
-    }
-}
-BENCHMARK(BM_cpu_func);
-
-static void BM_gpu_func(benchmark::State& state)
-{
-    for (auto _ : state) {
-        gpu_func();
-    }
-}
-BENCHMARK(BM_gpu_func);
-
-
 BENCHMARK_MAIN();
-
-
-
-
-
